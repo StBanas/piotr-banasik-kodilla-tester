5ee378b2de6f62a49e062a67e7e1637d42987308
StBanas <pio.banasik@gmail.com>
2020-03-15 19:55:39 +0100

    Zgodnie ze wskazówkami wprowadziłem zmianę (uproszczenie) warunku if w klasie LeapYear.java.
    Signed-off-by: StBanas <pio.banasik@gmail.com>

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -4,18 +4,20 @@ public class LeapYear {
     public LeapYear(int year){
         this.year = year;
     }
-
     public boolean checkYear(){
-        boolean isLeapYear;
-
-        if ((year % 4 == 0) && (year % 400 == 0 || year % 100 != 0)){
-            isLeapYear = true;
-        }
-        else{
-            isLeapYear = false;
-        }
-        return isLeapYear;
-        }
+        return (year % 4 == 0) && (year % 400 == 0 || year % 100 != 0);
+    }
+//    public boolean checkYear(){
+//        boolean isLeapYear;
+//
+//        if ((year % 4 == 0) && (year % 400 == 0 || year % 100 != 0)){
+//            isLeapYear = true;
+//        }
+//        else{
+//            isLeapYear = false;
+//        }
+//        return isLeapYear;
+//        }
 
 
 }
