7a8c602a2063240d4523cd8c6faad9f635506c9a
StBanas <pio.banasik@gmail.com>
2020-03-29 18:57:07 +0200

    User + Random Numbers - po poprawkach;
    Signed-off-by: StBanas <pio.banasik@gmail.com>

--- src/main/java/RandomNumbers.java
+++ src/main/java/RandomNumbers.java
@@ -2,35 +2,33 @@ import java.util.Random;
 
 public class RandomNumbers {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         int result;
         result = getCountOfRandomNumber(5000);
         System.out.println(" Ilość losowań =  " + result);
-
     }
 
-    public static int getCountOfRandomNumber(int max) {
+    public static int getCountOfRandomNumber(int max)
+    {
         Random random = new Random();
         int result = 0;
         int sum = 0;
         int tempMax = 0;
         int tempMin = 30;
-        while (sum < max) {
+        while (sum < max)
+        {
             int temp = random.nextInt(31);
             if (temp > tempMax) {tempMax = temp;}
             if (temp < tempMin) {tempMin = temp;}
 
-//            System.out.println(tempMax + " "+ temp + " "+ sum + " " + tempMin );
             sum = sum + temp;
             result++;
+        }
 
-
-            }
         System.out.println(" Najmniejsza wylosowana wartość = " + tempMin);
         System.out.println(" Największajsza wylosowana wartość = " + tempMax);
         return result;
-        }
-
-
+    }
 }
 
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -11,37 +11,31 @@ public class User {
 
     public static void main(String[] args){
 
-        User[] user = new User[6]; // nie wiem po co mi tu [6] ale inaczej nie dziiała?
+        User[] user = new User[5];
 
         String[] name  = {"Ala", "Ela", "Ola", "Ula", "C'la"};
         int[] age = {21, 23, 12, 31, 18};
         int i;
 
-        for (i = 1; i<6; i++)
+        for (i = 0; i<user.length; i++)
         {
-            user[i] = new User(name[i-1], age[i-1]);
+            user[i] = new User(name[i], age[i]);
             System.out.println(  " Imię : " + user[i].name + " ,  lat  :  " + user[i].age );
         }
+
         System.out.println("Srednia wieku wynosi :   " + sredniaWieku(age) );
 
         for(i = 1; i <user.length ; i++)
             if (user[i].age < sredniaWieku(age)) {
                 System.out.println(" Użytkownik o wieku poniżej średniej; " + user[i].name);
             }
-
     }
     public static int sredniaWieku(int[] age) {
         int sumaWieku = 0;
-        for (int i=0; i < age.length; i++){
-            sumaWieku += age[i];
+        for (int value : age) {
+            sumaWieku += value;
         }
-        int sredniaWieku = sumaWieku / age.length;
-
-        return sredniaWieku;
+        return sumaWieku / age.length;
     }
-
-
-
-
 }
 
