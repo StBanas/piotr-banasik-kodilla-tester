74340ac6e05e309ceef99a53d6ba542243f88c27
StBanas <pio.banasik@gmail.com>
2020-04-25 11:13:27 +0200

    Stream - no of posts; first attempt;
    Signed-off-by: StBanas <pio.banasik@gmail.com>

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
@@ -0,0 +1,32 @@
+package com.kodilla.stream.homework;
+
+import com.kodilla.stream.UsersRepository;
+
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class ForumStats {
+    public static void main(String[] args) {
+        double avgOverForty = UsersRepository.getUsersList()
+                .stream()
+                .filter(u -> (u.getAge() >=40))
+                .map(u -> u.getNumberOfPosts())
+                .mapToInt(u -> u)
+                .average()
+                .getAsDouble();
+
+        System.out.println(avgOverForty);
+
+        double avgBeloveForty = UsersRepository.getUsersList()
+                .stream()
+                .filter(u -> (u.getAge() < 40))
+                .map(u -> u.getNumberOfPosts())
+                .mapToInt(u -> u)
+                .average()
+                .getAsDouble();
+
+        System.out.println(avgBeloveForty);
+
+    }
+
+}
