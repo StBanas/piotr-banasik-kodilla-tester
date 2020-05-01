43fa4c6096276d4abfb89138c40478c987533728
StBanas <pio.banasik@gmail.com>
2020-05-01 09:32:24 +0200

    UserValidatorTestSuite - sorted out after Mentor's comments;
    Signed-off-by: StBanas <pio.banasik@gmail.com>

--- kodilla-advanced-tests/src/test/java/com/kodilla/paramertized_tests/homework/UserValidatorTestSuit.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/paramertized_tests/homework/UserValidatorTestSuit.java
@@ -3,12 +3,13 @@ package com.kodilla.paramertized_tests.homework;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UserValidatorTestSuit {
 
     @ParameterizedTest
-    @CsvSource(value = {"Jan Kowal", "jankowal", "_jan.Kowal_"}) //}
+    @CsvSource(value = {"JanKowal", "jankowal", "_jan.Kowal_"}) //}
 
     public void shouldUsernamrMatchRegEx(String username) {
         UserValidator userValidator = new UserValidator();
@@ -17,8 +18,25 @@ public class UserValidatorTestSuit {
 
     }
     @ParameterizedTest
-    @CsvSource(value = {"111111", "wel.blad@yahoo.com", "q@q.w"})
-    public void shouldSmailMatchRegEx(String username) {
+    @CsvSource(value = {"Jan Kowal", "~jankowal", "=jan.Kowal="}) //}
+
+    public void shouldNotUsernamrMatchRegEx(String username) {
+        UserValidator userValidator = new UserValidator();
+        boolean result = userValidator.validateUsername(username);
+        assertFalse(result);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"111111", "more.wel.blad@ya@oo.com", "qq.walter&yahoo.com"})
+    public void shouldNotEmailMatchRegEx(String username) {
+        UserValidator validateEmail = new UserValidator();
+        boolean result1 = validateEmail.validateEmail(username) ;
+        assertFalse(result1);
+
+    }
+    @ParameterizedTest
+    @CsvSource(value = {"12.11@11.com", "wel.blad@yahoo.com", "q@q.w"})
+    public void shouldEmailMatchRegEx(String username) {
         UserValidator validateEmail = new UserValidator();
         boolean result1 = validateEmail.validateEmail(username) ;
         assertTrue(result1);
