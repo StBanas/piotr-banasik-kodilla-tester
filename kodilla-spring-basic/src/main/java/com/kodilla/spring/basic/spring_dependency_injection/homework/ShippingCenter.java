d63b1357a730319cbd73e0c99ea11d7c1bc7c7e0
StBanas <pio.banasik@gmail.com>
2020-05-14 18:57:11 +0200

    Shipping Center - Spring - 2nd edition;
    Signed-off-by: StBanas <pio.banasik@gmail.com>

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework/ShippingCenter.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework/ShippingCenter.java
@@ -2,12 +2,21 @@ package com.kodilla.spring.basic.spring_dependency_injection.homework;
 
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
+
 @Component
 public class ShippingCenter {
-//    @Resource(name = "notificationService")
 
-    private DeliveryService deliveryService;
+    @Resource(name = "notificationService")
     private NotificationService notificationService;
+
+    @Resource(name = "deliveryService")
+    private DeliveryService deliveryService;
+
+//    public ShippingCenter(DeliveryService deliveryService, NotificationService notificationService) {
+//        this.deliveryService = deliveryService;
+//        this.notificationService = notificationService;
+//    }
 
     public String sendPackage(String address, double weight) {
         if (deliveryService.deliverPackage(address, weight)) {
