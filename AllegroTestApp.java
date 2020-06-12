6d9d1dc9e291dfe1ed2a6b3f6d3547a1822758ad
StBanas <pio.banasik@gmail.com>
2020-06-12 19:35:38 +0200

    Add files via upload
    AllegroTestApp - upload from local repository;

---
+++ AllegroTestApp.java
@@ -0,0 +1,41 @@
+package com.kodilla.selenium.allegro;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AllegroTestApp {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/home/stbanas/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.allegro.pl/");
+
+        WebElement inputItem;
+
+        inputItem = driver.findElement(By.xpath("//html/body/div[2]/div[3]/header/div/div/div/div/form/div[2]/div/select"));
+        inputItem.sendKeys("Elektronika");
+        inputItem = driver.findElement(By.xpath("//html/body/div[2]/div[3]/header/div/div/div/div/form/input"));
+        inputItem.sendKeys("mavic mini");
+        inputItem.submit();
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//
+//        inputItem = driver.findElement(By.xpath("//html/body/div[2]/div[3]/header/div/div/div/div/form/input"));
+//        inputItem.sendKeys("mavic mini");
+//
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        inputItem = driver.findElement(By.xpath("//html/body/div[2]/div[2]/header/div/div/div/div/form/div[2]/div/select"));
+//        inputItem.sendKeys("Elektronika");
+//        inputItem.submit();
+    }
+}
+
