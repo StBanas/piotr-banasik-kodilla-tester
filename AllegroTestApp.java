cf35ecdbe4cf1c851d4d96e73c89f4aaea7b717b
StBanas <pio.banasik@gmail.com>
2020-06-12 19:43:48 +0200

    AllegroTestApp - uploaded from local repository;
    This class was created in InleiJ at  Windows 10 OS;

--- AllegroTestApp.java
+++ AllegroTestApp.java
@@ -8,34 +8,27 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class AllegroTestApp {
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver", "/home/stbanas/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/selenium-drivers/Chrome/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.allegro.pl/");
 
-        WebElement inputItem;
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-        inputItem = driver.findElement(By.xpath("//html/body/div[2]/div[3]/header/div/div/div/div/form/div[2]/div/select"));
+        WebElement inputItem = driver.findElement(By.xpath("/html/body/div[2]/div[2]/header/div/div/div/div/form/div[2]/div/select"));
         inputItem.sendKeys("Elektronika");
-        inputItem = driver.findElement(By.xpath("//html/body/div[2]/div[3]/header/div/div/div/div/form/input"));
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        inputItem = driver.findElement(By.xpath("/html/body/div[2]/div[2]/header/div/div/div/div/form/input"));
         inputItem.sendKeys("mavic mini");
         inputItem.submit();
-//        try {
-//            Thread.sleep(5000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//
-//        inputItem = driver.findElement(By.xpath("//html/body/div[2]/div[3]/header/div/div/div/div/form/input"));
-//        inputItem.sendKeys("mavic mini");
-//
-//        try {
-//            Thread.sleep(5000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        inputItem = driver.findElement(By.xpath("//html/body/div[2]/div[2]/header/div/div/div/div/form/div[2]/div/select"));
-//        inputItem.sendKeys("Elektronika");
-//        inputItem.submit();
     }
 }
-
